0ab908f135501bfccbccca21197813db664d05f8
qawi <qawitherev@gmail.com>
2022-09-20 14:35:25 +0800

    completed ui for need assistance activity and extend parking activity

---
+++ app/src/main/java/com/abing/letak/needassistance/NeedAssistanceActivity.kt
@@ -0,0 +1,12 @@
+package com.abing.letak.needassistance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.abing.letak.R
+
+class NeedAssistanceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_need_assistance)
+    }
+}
